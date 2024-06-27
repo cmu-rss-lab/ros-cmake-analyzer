ef7fd99d47912639c844f248cc4af187929955a8
Bradley Schmerl <schmerl@cs.cmu.edu>
2024-06-27 16:24:21 -0400

    Fix use of defusedxml

--- src/ros_cmake_analyzer/core/nodelets_xml.py
+++ src/ros_cmake_analyzer/core/nodelets_xml.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-import defusedxml as dom
+from defusedxml import ElementTree as ET
 from loguru import logger
 
 
@@ -33,21 +33,19 @@ class NodeletsInfo:
     def from_nodelet_xml(cls, contents: str) -> "NodeletsInfo":
         libraries: list["NodeletLibrary"] = []
         contents = "<root>\n" + contents + "\n</root>"
-        tree = dom.parseString(contents)
+        tree = ET.fromstring(contents)
         root = get_xml_nodes_by_name("root", tree)[0]
         libraries_dom = get_xml_nodes_by_name("library", root)
         if not libraries_dom:
             logger.warning("Expected there to be <library/> elements in nodelet_plugins.xml, but there are none.")
             logger.debug(contents)
         for library_dom in libraries_dom:
-            assert isinstance(library_dom, dom.Element)
             path = library_dom.getAttribute("path")
             class_doms = get_xml_nodes_by_name(
                 "class",
                 library_dom,
             )
             for class_dom in class_doms:
-                assert isinstance(class_dom, dom.Element)
                 name = class_dom.getAttribute("name")
                 type_ = class_dom.getAttribute("type")
                 libraries.append(NodeletLibrary(path=path,
@@ -57,6 +55,6 @@ class NodeletsInfo:
         return NodeletsInfo(libraries=libraries)
 
 
-def get_xml_nodes_by_name(tag_name: str, tree: dom.Node) -> list[dom.Node]:
+def get_xml_nodes_by_name(tag_name: str, tree: ET) -> list[ET]:
     return [n for n in tree.childNodes
             if n.nodeType == n.ELEMENT_NODE and n.tagName == tag_name]
